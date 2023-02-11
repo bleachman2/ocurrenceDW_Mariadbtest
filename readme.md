00d637ea4f47086c9f2eed2b44879a4ea9fca883
Nelson Sousa <bleachman@live.com.pt>
2023-02-11 17:06:01 +0000

    basic dump, and pre-commit

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-Training with the https://dados.gov.pt/pt/datasets/prociv-ocorrencias-em-aberto/#_ dataset
+Using some of the open data from dados.gov
+Crimes registados n pelas autoridades policiais - https://dados.gov.pt/pt/datasets/r/51d09405-bc96-4bd4-80dd-5160d9f0e111
+
+
+## Todo
+pre-commit hook with flake8 and black
+load more data -> maybe freguesias https://dados.gov.pt/pt/datasets/r/ec6ef805-c278-4b4d-ba9b-3116264f68b4
+load existing data into mariadb
+format data into DW compatability
+frontend
+add type hint
--- scripts/downloadDump.py
+++ scripts/downloadDump.py
@@ -2,17 +2,19 @@ import requests
 import configparser
 import csv
 
+
 def load_data():
     config = configparser.ConfigParser()
-    config.read('scripts/config.cfg')
-    crimeURL = config['URL']['crimesRegistadosURL']
-    ## Dados de 2021
-    r= requests.get(crimeURL).json()[0]['Dados']['2021']
-    with open('data/crimeData.csv', 'w') as file:
-        ## get all unique keys 
+    config.read("scripts/config.cfg")
+    crimeURL = config["URL"]["crimesRegistadosURL"]
+    # Dados de 2021
+    r = requests.get(crimeURL).json()[0]["Dados"]["2021"]
+    with open("data/crimeData.csv", "w") as file:
+        # get all unique keys
         header = {head for ocurrence in r for head in ocurrence.keys()}
-        writer = csv.DictWriter(file,fieldnames=header)
+        writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
         writer.writerows(r)
 
-load_data())
+
+load_data()
